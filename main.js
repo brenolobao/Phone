56c7fbaa0fb78b60280af7062e56b60500d8d7ef
Breno Lobão Silva Barbosa <80777991+brenolobao@users.noreply.github.com>
2022-11-25 14:15:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector('.item-1').addEventListener('click', function () {
+    document.querySelector('.select').style.left = "0%";
+});
+document.querySelector('.item-2').addEventListener('click', function () {
+    document.querySelector('.select').style.left = "20%";
+});
+document.querySelector('.item-3').addEventListener('click', function () {
+    document.querySelector('.select').style.left = "40%";
+});
+document.querySelector('.item-4').addEventListener('click', function () {
+    document.querySelector('.select').style.left = "60%";
+});
+document.querySelector('.item-5').addEventListener('click', function () {
+    document.querySelector('.select').style.left = "80%";
+});
